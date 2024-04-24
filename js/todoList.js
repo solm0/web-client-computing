50508e0a95d86e062c809bde1f8c2e9a6a7e4a32
정솔미 <solmi-@kookmin.ac.kr>
2024-04-24 15:48:57 +0900

    0424

---
+++ js/todoList.js
@@ -0,0 +1,43 @@
+const title = document.querySelector("#title");
+const comments = document.querySelector("#comments");
+const save = document.querySelector("#save");
+const todoList = document.querySelector("#todoList");
+
+save.addEventListener("click", (e) => {
+
+    e.preventDefault();
+
+    const item = document.createElement("li");
+    item.innerHTML = `${title.value} - ${comments.value} <span class = "delButton">Delete</span>`;
+    todoList.appendChild(item);
+
+    title.value = "";
+    comments.value = "";
+
+    const delButtons = document.querySelectorAll(".delButton");
+    const body = document.querySelector("body");
+
+    for (let delButton of delButtons) {
+        delButton.addEventListener("click", function() {
+            this.parentNode.remove(this.parentNode);
+        });
+        delButton.addEventListener("mouseover", (e) => {
+            
+            var count = 0;
+            delButton.style.fontSize = "50px";
+            delButton.innerText = "really?";
+            body.style.backgroundColor = "grey";
+            count++;
+
+            delButton.addEventListener("mouseout", (e) => {
+                delButton.innerText = "did you finished???"
+                delButton.style.color = "red";
+                body.style.backgroundColor = "black";
+            });
+            console.log("mouseover")
+        });
+    }
+
+
+
+});
